a7a268a597eab28dc26f73ae544643de322bbf01
Oleg Vaskevich <oleg_vas08@mail.ru>
2020-09-10 22:16:13 +0300

    fix: fix function transform(arr), not done yet.

--- src/transform-array.js
+++ src/transform-array.js
@@ -14,7 +14,7 @@ const dblNext = arrKey[2];
 const dblPrev = arrKey[3];
 
 let copyArr;
-
+let finelarr;
 
 
 module.exports = function transform(arr) {
@@ -22,13 +22,22 @@ module.exports = function transform(arr) {
   // throw new CustomError('Not implemented');
   // remove line with error and write your code here
 
-//let a=[b] ; //[ '--discard-next', 'DEF' ];
 
-//transform(a);   //[ Array(8) ]
 
-//function transform(arr) {
+  /*
+
+let a=[ 'DEF', 1, '--double-prev' ] ; //[ '--discard-next', 'DEF' ];
+
+transform(a);   //[ Array(8) ]
+
+function transform(arr) {
+
+
+*/
+
   //console.log(arr);
-  copyArr=arr.slice();
+  //copyArr=arr.slice();
+  copyArr=deepCopyFunction(arr);
 
   /*
   if(is2dArray(arr)){
@@ -63,7 +72,7 @@ function getKeyAndPos(arr){
       if(!(typeof elem ==='string')){
         continue;
       }
-      temp.push([elem, arr.indexOf(elem)]);
+      temp.push([deepCopyFunction(elem), arr.indexOf(elem)]);
       flag=1;
     } 
   }
@@ -75,39 +84,49 @@ function getKeyAndPos(arr){
 
 function disNextF(pos){
   if(copyArr.length-1==pos){
-    copyArr[pos]='del';
+    copyArr.splice(pos,1,'del');
+    //copyArr[pos]='del';
     return;
   }
-  copyArr[pos]='del';
-  copyArr[pos+1]='del';
+  copyArr.splice(pos,1,'del');
+  copyArr.splice(pos+1,1,'del');
+ // copyArr[pos]='del';
+  //copyArr[pos+1]='del';
   return;
 }
 
 function disPrevF(pos){
   if(pos==0){
-    copyArr[pos]='del';
+    copyArr.splice(pos,1,'del');
+    //copyArr[pos]='del';
     return;
   }
-  copyArr[pos]='del';
-  copyArr[pos-1]='del';
+  copyArr.splice(pos,1,'del');
+  copyArr.splice(pos-1,1,'del');
+  //copyArr[pos]='del';
+  //copyArr[pos-1]='del';
   return;
 }
 
 function dblNextF(pos){
   if(copyArr.length-1==pos){
-    copyArr[pos]='del';
+    copyArr.splice(pos,1,'del');
+    //copyArr[pos]='del';
     return;
   }
-  copyArr[pos]=copyArr[pos+1];//copyArr.slice(pos+1, pos+2);
+  copyArr.splice(pos,1,deepCopyFunction(copyArr[pos+1]));
+  //copyArr[pos]=deepCopyFunction(copyArr[pos+1]);//copyArr.slice(pos+1, pos+2);
   return;
 }
 
 function dblPrevF(pos){
   if(pos==0){
-    copyArr[pos]='del';
+    copyArr.splice(pos,1,'del');
+    //copyArr[pos]='del';
     return;
   }
-  copyArr[pos]=copyArr[pos-1];//copyArr.slice(pos-1, pos);
+  copyArr.splice(pos,1,deepCopyFunction(copyArr[pos-1]));
+  //copyArr[pos]=deepCopyFunction(copyArr[pos-1]);//copyArr.slice(pos-1, pos);
   return;
 }
 
@@ -133,23 +152,29 @@ function callFunc(elem){
 
 function finalArr(arr){
   let temp=[];
-  /*
+  
   for(elem of arr){
     
     if(elem!='del'){
       //let el=arr.slice(arr.indexOf(elem),1);
-      temp.push(elem);
+      temp.push(deepCopyFunction(elem));
     }
     
   }
-  */
+  
+  /*
   let j=0;
   for(let i=0;i<arr.length;i++){
     if(arr[i]!='del'){
       temp[j]=arr[i];
       j++;
     }
+
   }
+  if(temp.length<=0){
+    return [];
+  }
+  */
   return temp;
 }
 
@@ -162,13 +187,41 @@ function is2dArray(array){
 }
 
 function check(arrOfKeys){
-  for(let i=0;i<arrOfKeys.length;i++){
+  for(let i=0;i<arrOfKeys.length-1;i++){
     if(//(arrOfKeys[i]==dblNext&&arrOfKeys[i+2]==disPrev)||
-       (arrOfKeys[i]==disNext&&arrOfKeys[i+2]==disPrev)||
-       (arrOfKeys[i]==disNext&&arrOfKeys[i+2]==dblPrev)){
-      copyArr[i+2]='del';
+       (arrOfKeys[i]==disNext&&arrOfKeys[i+2]==disPrev)//||
+      // (arrOfKeys[i]==disNext&&arrOfKeys[i+2]==dblPrev)
+      ){
+      //copyArr[i+2]='del';
+      copyArr.splice((i+2),1,'del');
       arrOfKeys[i+2]='';
       }
 
     }
+  } 
+  /* I'll be honest with you, I didn't create this funtion by myself
+     the deepCopy is too deep shit for me right now, though I figured out that there is a problem
+     with my arrays because of copying. Cause I saw that in console.log I have a good result, but
+     on test on the same array I can see those --dbl-nxt elements that I delited.
+     I Googled for deep copy; 
+     https://medium.com/javascript-in-plain-english/how-to-deep-copy-objects-and-arrays-in-javascript-7c911359b089
+  */
+  const deepCopyFunction = (inObject) => {
+    let outObject, value, key
+  
+    if (typeof inObject !== "object" || inObject === null) {
+      return inObject // Return the value if inObject is not an object
+    }
+  
+    // Create an array or object to hold the values
+    outObject = Array.isArray(inObject) ? [] : {}
+  
+    for (key in inObject) {  
+      value = inObject[key]
+  
+      // Recursively (deep) copy for nested objects, including arrays
+      outObject[key] = deepCopyFunction(value)
+    }
+  
+    return outObject
   } 
