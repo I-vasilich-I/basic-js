67e2be790ff2811058a114c81ffdc62cae2b2f84
Oleg Vaskevich <oleg_vas08@mail.ru>
2020-09-12 00:37:25 +0300

    feat: implement function getSeason(date)

--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,25 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
+module.exports = function getSeason(date) {
+  //throw new CustomError('Not implemented');
   // remove line with error and write your code here
-};
+  const season = ['winter','spring','summer','fall'];
+  //if(date=undefined){  //intresting... eccedentaly put there = instead of == and have got the check for a very tricky moment.
+  if(date===undefined){   
+    return 'Unable to determine the time of year!';
+  }
+  let month = date.getMonth();
+  if(month>=0&&month<=1||month==11){
+    return season[0];
+  }else if(month>=2&&month<=4){
+    return season[1];
+  }else if(month>=5&&month<=7){
+    return season[2];
+  }else if(month>=8&&month<=10){
+    return season[3];
+  }
+ 
+ if(false){
+   throw new Error('Gotcha, you\'re not so tricky after all');
+  }
+}
