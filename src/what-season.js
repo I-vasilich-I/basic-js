24caa03ee153c485bd0cac7003783b8941d4881e
Oleg Vaskevich <oleg_vas08@mail.ru>
2020-09-12 21:38:33 +0300

    feat: implement function validatDate()

--- src/what-season.js
+++ src/what-season.js
@@ -7,8 +7,19 @@ module.exports = function getSeason(date) {
   if(date===undefined){   
     return 'Unable to determine the time of year!';
   }
-  date.getUTCDate();
-  //isArealDate(date);  //leave it there, 'cause why not?
+  validateDate(date);
+  /*
+  JSON.stringify(date); works too. JSON notes:
+  If the value has a toJSON() method, it's responsible to define what data will be serialized. 
+  The instances of Date implement the toJSON() function 
+  by returning a string (the same as date.toISOString()). Thus, they are treated as strings.
+
+  date.getUTCDate(); works too as a validator on this particular test. 
+  yeah I'm not throwing error by myself, but since there is no such method in fake Date, 
+  Error thrown by itself, at least I think so)
+
+  But we are here to study, that's why I'm putting the proper way to handle wrong/fake Date. 
+  */
   let month = date.getMonth();
   if(month>=0&&month<=1||month==11){
     return season[0];
@@ -20,10 +31,9 @@ module.exports = function getSeason(date) {
     return season[3];
   }
 }
-/*
-function isArealDate(date){
-  JSON.stringify(date); //worked too.
-  date.toString();  //this one doesn't work, probably because the method toString() was defined on fake Date. 
-  date.getUTCDate(); //working
+
+function validateDate(dateToValidate){
+  if(!(Object.prototype.toString.call(dateToValidate) === '[object Date]')){
+    throw new Error('Gotcha!');
+  }
 }
-*/
