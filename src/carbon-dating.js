22cb5b20a62571e07449f2bbe9bfba199b3378c1
Oleg Vaskevich <oleg_vas08@mail.ru>
2020-10-06 00:38:46 +0300

    fix: function according to new requirements

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -8,17 +8,13 @@ const bilion = 1000000000000000;
 module.exports = function dateSample(sampleActivity) {
   //throw new CustomError('Not implemented');
   // remove line with error and write your code here
-  if(sampleActivity==undefined||typeof sampleActivity!='string'||isNaN(sampleActivity)){
+  if(sampleActivity==undefined||typeof sampleActivity!='string'||isNaN(sampleActivity)
+    ||!isFinite(parseFloat(sampleActivity))){
     return false;
   }
-  //console.log(sampleActivity);
   let nN=(sampleActivity)/MODERN_ACTIVITY;
-  //console.log(nN);
   let k=(0.693*bilion)/HALF_LIFE_PERIOD;
- // console.log(k);
   let ln = (-1)*Math.log(nN);
-  //console.log(ln);
   let t = Math.ceil((ln/k)*bilion);
-  //console.log(t);
-  return t;
+  return !isFinite(t) || t < 0 || isNaN(t) ? false : t;
 };
