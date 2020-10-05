4340a18d832c2119d0e9817de1a387481437c023
Oleg Vaskevich <oleg_vas08@mail.ru>
2020-10-06 01:25:40 +0300

    feat: implement function repeater()

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,36 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
+
+module.exports = function repeater(str, options) {
+  //throw new CustomError('Not implemented');
   // remove line with error and write your code here
+  let res = '';
+  let subRes = '';
+  if(options.separator===undefined){
+    options.separator='+';
+  }
+  if (options.additionSeparator===undefined){
+    options.additionSeparator = '|';
+  }
+  if (str===null) {
+    str = 'null';
+  }
+  if(options.addition!==undefined){
+    subRes = subRepeater(options.addition, options.additionRepeatTimes, options.additionSeparator);
+  }
+  res = subRepeater(str+subRes, options.repeatTimes, options.separator);
+
+  return res;
 };
+
+function subRepeater(str, count, separator){
+  let res='';
+  while(count>1){
+    res+=str+separator;
+    count--
+  }
+  return res+=str;
+}
+
+
   
