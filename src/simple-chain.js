049fa9aab62211cc0c79fb8544a338de57d3ef7a
Oleg-Vaskevich <oleg_vas08@mail.ru>
2020-10-08 16:53:14 +0300

    feat: implement chainMaker

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,26 +1,44 @@
 const CustomError = require("../extensions/custom-error");
 
+
+
 const chainMaker = {
+  res: [],
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    return this.res.length
   },
   addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if (value === undefined) value = '';
+    if (value === null) value = 'null';
+    if (value === 0) value = '0';
+    this.res.push(`( ${value} )`);
+    //console.log(`addLink ${value}`, this.res);
+    return this;
   },
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if(this.getLength() < position || position < 0 || !(typeof position == 'number') ) {
+      throw new Error();
+    }
+    this.res.splice(position-1, 1);
+    //console.log(`removeLink ${position}`, this.res);
+    return this;
   },
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.res.reverse();
+    //console.log('reverseChain', this.res);
+    return this;
   },
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    let tempres = this.res.join('~~');
+    this.res.splice(0, this.getLength())
+    //console.log('finishChain', tempres);
+    //console.log(this.res.join('~~'))
+    return tempres;
   }
 };
 
+
+//chainMaker.reverseChain().reverseChain().addLink('DEF').addLink(NaN).reverseChain().addLink(333).reverseChain().addLink('GHI').addLink('ABC').addLink({0: 'first', 1: 'second', 'length': 2}).finishChain()
 module.exports = chainMaker;
+
+
