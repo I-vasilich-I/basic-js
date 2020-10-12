91b293d8d76e2bc782f68246e64c5ef5091d3d35
Oleg Vaskevich <oleg_vas08@mail.ru>
2020-10-12 20:49:24 +0300

    feat: vigenere-cipher's done

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,85 +1,84 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
+  constructor(instance = true) {
+    this.instance = instance;
+  }
 
   encrypt(string, key) {
     let res = '';
     if (string==undefined || key==undefined) {
       throw new Error();
     }
-    key = adjustKey(string, key);
-    string = adjustString(string);
+    key = this.adjustKey(string, key);
+    string = this.adjustString(string);
     let j=0;
     for (let i = 0; i<string.length; i++){
-      if (ascii(string[i])<65 || ascii(string[i])>90) {
+      if (this.ascii(string[i])<65 || this.ascii(string[i])>90) {
         j++
         res+=string[i];
         continue;
       }
-      res+=encryptLetter(string[i], key[i-j]);
+      res+=this.encryptLetter(string[i], key[i-j]);
     }
-    console.log(res);
-    return res;
+    return this.instance === true ? res : res.split('').reverse().join('');
   }    
   decrypt(string, key) {
-    //throw new CustomError('Not implemented');
     let res = '';
     if (string==undefined || key==undefined) {
       throw new Error();
     }
-    key = adjustKey(string, key);
-    string = adjustString(string);
+    key = this.adjustKey(string, key);
+    string = this.adjustString(string);
     let j=0;
     for (let i = 0; i<string.length; i++){
-      if (ascii(string[i])<65 || ascii(string[i])>90) {
+      if (this.ascii(string[i])<65 || this.ascii(string[i])>90) {
         j++
         res+=string[i];
         continue;
       }
-      res+=decryptLetter(string[i], key[i-j]);
+      res+=this.decryptLetter(string[i], key[i-j]);
     }
-    console.log(res);
-    return res;
+    return this.instance === true ? res : res.split('').reverse().join('');
+  }
+
+  adjustKey(string, key) {
+    if (key.length < string.length) {
+      let temp = key.padEnd(string.length, key);
+      return temp.toUpperCase();
+    }
+    return key.toUpperCase();
+  }
+
+  adjustString(string) {
+    return string.toUpperCase();
+  }
+
+  encryptLetter(strLetter, keyLetter) {
+    strLetter = this.ascii(strLetter)-65;
+    keyLetter = this.ascii(keyLetter)-65;
+    let sum = 65+strLetter+keyLetter;
+    let encrypted = sum > 90 ? sum-26 : sum;
+    encrypted = this.asciiLetter(encrypted);
+    return encrypted;
+  }
+
+  decryptLetter(strLetter, keyLetter) {
+    strLetter = this.ascii(strLetter)-65;
+    keyLetter = this.ascii(keyLetter)-65;
+    let sum = 26 + strLetter - keyLetter;
+    let decrypted = sum + 65 > 90 ? sum + 65 - 90 + 65 - 1 : sum + 65;
+    decrypted = this.asciiLetter(decrypted);
+    return decrypted;
+  }
+
+  ascii(letter) {
+    return letter.charCodeAt(0);
+  }
+
+  asciiLetter(code) {
+    return String.fromCharCode(code);
   }
 }
 
 module.exports = VigenereCipheringMachine;
-
-
-function adjustKey(string, key) {
-  if (key.length < string.length) {
-    let temp = key.padEnd(string.length, key);
-    return temp.toUpperCase();
-  }
-  return key.toUpperCase();
-}
-
-function adjustString(string) {
-  return string.toUpperCase();
-}
-
-function encryptLetter(strLetter, keyLetter) {
-  strLetter = ascii(strLetter)-65;
-  keyLetter = ascii(keyLetter)-65;
-  let sum = 65+strLetter+keyLetter;
-  let encrypted = sum > 90 ? sum-26 : sum;
-  encrypted = asciiLetter(encrypted);
-  return encrypted;
-}
-
-function decryptLetter(strLetter, keyLetter) {
-  strLetter = ascii(strLetter)-65;
-  keyLetter = ascii(keyLetter)-65;
-  let sum = 65+strLetter - keyLetter; //тута
-  let decrypted = sum;
-  decrypted = asciiLetter(decrypted);
-  return decrypted;
-}
-
-function ascii(letter) {
-  return letter.charCodeAt(0);
-}
-
-function asciiLetter(code) {
-  return String.fromCharCode(code);
-}
